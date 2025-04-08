fcf95a1274e0b285165d91608ced8f8755c062d3
Phelokazi-Dube <phelokazidube@gmail.com>
2025-04-08 20:21:09 +0200

    Updated a route

--- apps/fluffy_web/lib/fluffy_web/router.ex
+++ apps/fluffy_web/lib/fluffy_web/router.ex
@@ -42,7 +42,6 @@ defmodule FluffyWeb.Router do
     get("/contact", PageController, :home, private: %{:javascript => "contact"})
     get("/sites", PageController, :home, private: %{:javascript => "sites"})
     get("/downloading", PageController, :home, private: %{:javascript => "downloading_data"})
-    get("/survey", PageController, :home, private: %{:javascript => "surveys"})
     get("/auth/google/callback", GoogleAuthController, :index)
     get("/auth/google/page", PageController, :home, private: %{:javascript => "new"})
     get "/logout", GoogleAuthController, :logout
@@ -62,6 +61,7 @@ defmodule FluffyWeb.Router do
   scope "/", FluffyWeb do
     pipe_through(:admin_only)
     post "api/Mongodb/approve_document/:id", MongoDBController, :approve
+    get("/survey", PageController, :home, private: %{:javascript => "surveys"})
   end
 
   # Other scopes may use custom stacks.
