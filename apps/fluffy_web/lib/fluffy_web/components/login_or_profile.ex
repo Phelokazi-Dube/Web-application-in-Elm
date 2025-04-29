1495f5a8b8aa45bfbedc23e775c1e03dc334b945
Yusuf Motara <y.motara@ru.ac.za>
2025-04-29 12:39:19 +0200

    Small UI fixes, relying on custom components (though, really, it could all be done inline)

--- apps/fluffy_web/lib/fluffy_web/components/login_or_profile.ex
+++ apps/fluffy_web/lib/fluffy_web/components/login_or_profile.ex
@@ -7,16 +7,17 @@ defmodule FluffyWeb.LoginOrProfile do
   attr :oauth_url, :string, required: true
   def login_link(assigns) do
     ~H"""
-      <a href={@oauth_url}>Login</a>
+      <a href={@oauth_url} class="nav-link">LOGIN</a>
     """
   end
 
   attr :name, :string, required: true
   attr :picture, :string, required: true
   def profile(assigns) do
+    title = "Logged in as #{assigns.name}"
     ~H"""
-        Logged in as <%= @given_name %> <img width="32px" src={@picture} class="rounded-[50%] inline align-middle">
-        <a href="/logout" class="ml-2 text-red-500">Logout</a>
+        <a href="/logout" class="nav-link">LOGOUT</a>
+        <img width="32px" src={@picture} title={title} class="rounded-[50%] inline align-middle">
       """
   end
 
